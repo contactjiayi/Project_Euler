df41501c9fee731fca9402c3cd5770b09a8ccc5a
Home <contactjiayi@gmail.com>
2013-09-29 01:46:34 -0700

    added alternative implementation making use of prime factors

--- euler_problem005.py
+++ euler_problem005.py
@@ -3,22 +3,55 @@
 # What is the smallest positive number that is evenly divisible by all of the 
 # numbers from 1 to 20?
 
-num = 1
-multiples_list = []
+# alternative implementation making use of prime factors
+def isPrime(number):
+	for i in range(2, number):
+		if number % i == 0:
+			return False
+	return True
 
-for divisor in range(1,21):
-	multiples_list.append(divisor)
+primes = []
+for i in range(2,21):
+	if isPrime(i):
+		primes.append(i)
 
-index = len(multiples_list) - 2
+factors = []
+count = 1
 
-while index > 0:
-	multiple = 20 * num
+for i in primes:
+	while i ** count < 20:
+		factors.append(i)
+		count += 1
 
-	if multiple % multiples_list[index] == 0:
-		index -= 1
-	else:
-		num += 1
-		index = len(multiples_list) - 2
+	if i ** count > 20:
+		count = 1
 
-if index == 0:
-	print(multiple)
+print(factors)
+
+smallest_multiple = 1
+
+for j in factors:
+	smallest_multiple *= j
+
+print(smallest_multiple)
+
+# original implementation
+# num = 1
+# multiples_list = []
+
+# for divisor in range(1,21):
+# 	multiples_list.append(divisor)
+
+# index = len(multiples_list) - 2
+
+# while index > 0:
+# 	multiple = 20 * num
+
+# 	if multiple % multiples_list[index] == 0:
+# 		index -= 1
+# 	else:
+# 		num += 1
+# 		index = len(multiples_list) - 2
+
+# if index == 0:
+# 	print(multiple)
